93c8385f02cfae234b3aeac7af3132495351f8b8
Cinthia Lindolfo <cinthia20lindolfo@gmail.com>
2025-09-26 09:05:48 -0300

    feat: model: adicionado Coleta

--- src/main/java/cinthia/lindolfo/ecolete/model/Coleta.java
+++ src/main/java/cinthia/lindolfo/ecolete/model/Coleta.java
@@ -1,5 +1,88 @@
 package cinthia.lindolfo.ecolete.model;
 
+import java.time.LocalDateTime;
+
+import jakarta.persistence.*;
+import jakarta.validation.constraints.NotEmpty;
+import jakarta.validation.constraints.NotNull;
+
+@Entity
+@Table(name = "tb_coletas")
 public class Coleta {
-    
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+
+    @NotEmpty(message = "Descrição do material")
+    private String descricaoMaterial;
+
+    @NotNull(message = "Quantidade é obrigatória")
+    private Integer quantidadeMaterial;
+
+    @NotNull(message = "Data de agendamento é obrigatória")
+    private LocalDateTime dataAgendamento;
+
+    private LocalDateTime dataConclusao;
+    private String status = "AGENDADA";
+
+    @ManyToOne
+    @JoinColumn(name = "usuario_id")
+    private Usuario usuario;
+
+    // Getters e Setters
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getDescricaoMaterial() {
+        return descricaoMaterial;
+    }
+
+    public void setDescricaoMaterial(String descricaoMaterial) {
+        this.descricaoMaterial = descricaoMaterial;
+    }
+
+    public Integer getQuantidadeMaterial() {
+        return quantidadeMaterial;
+    }
+
+    public void setQuantidadeMaterial(Integer quantidadeMaterial) {
+        this.quantidadeMaterial = quantidadeMaterial;
+    }
+
+    public LocalDateTime getDataAgendamento() {
+        return dataAgendamento;
+    }
+
+    public void setDataAgendamento(LocalDateTime dataAgendamento) {
+        this.dataAgendamento = dataAgendamento;
+    }
+
+    public LocalDateTime getDataConclusao() {
+        return dataConclusao;
+    }
+
+    public void setDataConclusao(LocalDateTime dataConclusao) {
+        this.dataConclusao = dataConclusao;
+    }
+
+    public String getStatus() {
+        return status;
+    }
+
+    public void setStatus(String status) {
+        this.status = status;
+    }
+
+    public Usuario getUsuario() {
+        return usuario;
+    }
+
+    public void setUsuario(Usuario usuario) {
+        this.usuario = usuario;
+    }
 }
